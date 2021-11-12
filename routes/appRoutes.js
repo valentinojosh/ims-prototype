236b785fdc5ea71f6260f575babbea45ea297fce
Andy Garcia <75452731+GrandDukeX@users.noreply.github.com>
2021-11-12 10:27:50 -0600

    Renamed files to fit the description of the files

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (app) {
-    var todoList = require('../controller/appController');
+    var todoList = require('../controller/minorLogAppController');
     // todoList Routes
     app.route('/fvsra/minorInjuryLog')
         .get(todoList.list_all_minorIncidents)
@@ -9,4 +9,5 @@ module.exports = function (app) {
     app.route('/fvsra/minorInjuryLog/:minor_injury_id')
         .get(todoList.read_a_minorIncident)
         .delete(todoList.deleteMinorIncident)
+        .put(todoList.updateMinorIncident)
 };
